808ac5b2662449e8dbc9b80cdaaa42eb6d58ebdb
fayiz-123 <muhammedfayiz59@gmail.com>
2025-10-10 17:09:04 +0530

    - Created BlogDetails Page for Showing details of each blogs - Created config.js file for storing the urls. - Configure Starpi and added new fields and components and updated image fields. - Rename File Namings.

---
+++ frontend/config.js
@@ -0,0 +1,6 @@
+const CONFIG = {
+  API_BASE_URL: "http://localhost:1337/api",  // For API requests
+  BASE_URL: "http://localhost:1337",          // For static/media URLs
+  PLACEHOLDER_IMAGE: "https://via.placeholder.com/400x300?text=No+Image",
+  MODE: "development"
+};
