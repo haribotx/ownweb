04d5b016ab00fd4f9f28cbf564d029ac20422813
libinkallingal <libinbotxBOTXSWe2403@gmail.com>
2025-10-21 18:29:44 +0530

    scrollsectionupdate

--- frontend/config.js
+++ frontend/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
-  // API_BASE_URL: "http://localhost:1337/api",  // For API requests
+  API_BASE_URL: "http://localhost:1337/api", 
   // API_BASE_URL: "https://tremendous-sunrise-a4f793fe32.strapiapp.com/api",
-  API_BASE_URL: "https://informed-actor-c1c7a2747a.strapiapp.com/api",
+  // API_BASE_URL: "https://informed-actor-c1c7a2747a.strapiapp.com/api",
   BASE_URL: "http://localhost:1337",          // For static/media URLs
   PLACEHOLDER_IMAGE: "https://via.placeholder.com/400x300?text=No+Image",
   MODE: "development"
